b94bd6ffbfd27a26e352523b4d128c1d68b9e5ed
fbianchis <54005086+fbianchis@users.noreply.github.com>
2020-01-27 09:35:44 -0300

    finalização do modulo js

---
+++ modulo4_js/aula3.js
@@ -0,0 +1,9 @@
+axios.get('https://api.github.com/users/fbianchis')
+  .then(function(response)
+  {
+    console.log(response);
+  })
+  .catch(function(error)
+  {
+    console.warn(error);
+  });
